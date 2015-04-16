a1efb17af83ea854c05b00263af461b99c4dde55
Elena Danilova <elena.danilowna@gmail.com>
2015-04-16 18:22:39 +0300

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Elena Danilowa"
+    email    "elena.danilowna@gmail.com"
+    password "malaya"
+    password_confirmation "malaya"
+  end
+end
